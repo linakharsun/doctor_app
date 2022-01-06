97a36c395f2f071123d3d158f7bd092eb2d9308e
Angelina Kardakova <jenta.fra.sibir@gmail.com>
2022-01-06 21:03:59 +0700

    first commit

---
+++ lib/screens/auth/components/sign_up_form.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import '../../../constants.dart';
+
+class SignUpForm extends StatelessWidget {
+  SignUpForm({
+    Key? key,
+    required this.formKey,
+  }) : super(key: key);
+
+  final GlobalKey formKey;
+
+  late String _userName, _email, _phoneNumber, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextFieldName(text: "Username"),
+          TextFormField(
+            decoration: InputDecoration(hintText: "Placeholder"),
+            validator: RequiredValidator(errorText: "Username is required"),
+            onSaved: (username) => _userName = username!,
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFieldName(text: "Email"),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(hintText: "tast@email.com"),
+            validator: EmailValidator(errorText: "Use a valide email address"),
+            onSaved: (email) => _email = email!,
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFieldName(text: "Phone"),
+          TextFormField(
+            keyboardType: TextInputType.phone,
+            decoration: InputDecoration(hintText: "+123456789"),
+            validator: RequiredValidator(errorText: "Phone number is required"),
+            onSaved: (phonaNumber) => _phoneNumber = phonaNumber!,
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFieldName(text: "Password"),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(hintText: "********"),
+            validator: passwordValidator,
+            onSaved: (password) => _password = password!,
+            onChanged: (pass) => _password = pass,
+          ),
+          const SizedBox(height: defaultPadding),
+          TextFieldName(text: "Confirm Password"),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(hintText: "********"),
+            validator: (pass) =>
+                MatchValidator(errorText: "Password doesn't match")
+                    .validateMatch(pass!, _password = pass),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TextFieldName extends StatelessWidget {
+  const TextFieldName({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: defaultPadding / 3),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          color: Colors.black54,
+        ),
+      ),
+    );
+  }
+}
